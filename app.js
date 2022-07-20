c763741e4ad332a795851627813445481f6fc5d2
HeitorBaraldi <carlos.hgb88@gmail.com>
2022-07-20 15:57:34 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sitema de gernciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 // mais um console log de teste 
 console.log("o Paulo fez esse commit sem você ver na aula!")
+
